7f8aa1a69bbc74bfadc1ddde64275f6b44d052ea
Prinku07 <96183069+Prinku07@users.noreply.github.com>
2024-04-05 17:55:19 +0530

    database connection done

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 //for recgnise env variables
 const dotenv = require('dotenv');
+
+const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' }); //
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
 //console.log(app.get('env')); console.log(process.env);
 
-console.log(process.env.PORT);
-console.log(process.env.NODE_ENV);
+// console.log(process.env.PORT);
+// console.log(process.env.NODE_ENV);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+  })
+  .then(() => console.log('DB Connection Successful'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
